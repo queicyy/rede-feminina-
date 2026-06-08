72bde87564cc2c0613e56e512ec1452ef3eb3d89
Queicy <queicyy@yahoo.com>
2026-06-08 16:05:22 -0300

    Configuração PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,35 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    legacy()
+    legacy(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'RFCC Itapema',
+        short_name: 'RFCC',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#e9b7c8',
+        icons: [
+          {
+            src: '/favicon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/favicon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   test: {
     globals: true,
