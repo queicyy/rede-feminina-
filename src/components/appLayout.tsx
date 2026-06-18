47887c332d40badf4fbffcb3cd4beac879bac023
Queicy <queicyy@yahoo.com>
2026-06-18 15:04:10 -0300

    Move menu lateral para esquerda

--- src/components/appLayout.tsx
+++ src/components/appLayout.tsx
@@ -1,32 +1,19 @@
-import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonButtons,
-  IonMenuButton,
-  IonTitle,
-  IonContent,
-  IonBackButton,
-} from '@ionic/react'
-
-import { chevronBack } from 'ionicons/icons'
+import { IonPage, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from "@ionic/react";
 
 export default function AppLayout({ title, children }: { title: string; children: React.ReactNode; history: any }) {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonButtons slot='start'>
-            <IonBackButton icon={chevronBack}></IonBackButton>
-          </IonButtons>
-          <IonTitle>{title}</IonTitle>
-          <IonButtons slot='end'>
+          <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
+          <IonTitle>{title}</IonTitle>
         </IonToolbar>
       </IonHeader>
 
       <IonContent fullscreen>{children}</IonContent>
     </IonPage>
-  )
+  );
 }
+  
